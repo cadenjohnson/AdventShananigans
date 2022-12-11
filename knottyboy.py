db2ba4fd0988e50191fafaaa4f187a10bd4166b9
cadenjohnson <caden82010@hotmail.com>
2022-12-11 18:16:29 -0500

    Knot an easy one...

--- knottyboy.py
+++ knottyboy.py
@@ -1,5 +1,14 @@
 
 
+def convert_to_one(num):
+    if num<0:
+        return -1
+    elif num>0:
+        return 1
+    else:
+        return 0
+
+
 class Knot:
     def __init__(self, head=None, tail=None):
         self.head = head
@@ -36,17 +45,19 @@ class Knot:
     def follow(self):
         distance_x = abs(self.head.location[0] - self.location[0])
         distance_y = abs(self.head.location[1] - self.location[1])
-        # diagonal move
-        if distance_x==2 and distance_y==2:
-            x = (self.head.location[0] - self.location[0])/2
-            y = (self.head.location[1] - self.location[1])/2
+        # diagonal move -- wow, so much effort put into this...
+        if distance_x + distance_y == 3:
+            x = convert_to_one(self.head.location[0] - self.location[0])
+            y = convert_to_one(self.head.location[1] - self.location[1])
             self.update_history()
             self.location = (self.location[0] + x, self.location[1] + y)
 
         # non-gay move
         elif distance_x > 1 or distance_y > 1:
             self.update_history()
-            self.location = self.head.past
+            x = convert_to_one(self.head.location[0] - self.location[0])
+            y = convert_to_one(self.head.location[1] - self.location[1])
+            self.location = (self.location[0]+x, self.location[1]+y)
 
 
 
@@ -68,10 +79,10 @@ for move in instructions:
         for knot in Knots:
             knot.lead_or_follow(direction, 1)
 
+
 # update their last and final positions
 for i in Knots:
         i.update_history()
 
 print(len(Knots[-1:][0].history))
-
 
